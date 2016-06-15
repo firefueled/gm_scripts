722c386654cc3bef9bbdd87766fe2a7899f41300
Pablo Soares <pablo@mw8solucoes.com.br>
2016-06-15 09:44:52 -0300

    Simply hiding the side panels

---
+++ bitrix_decrapifier.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        Bitrix decrapifier
+// @namespace   com.firefueled.biitrixdecrapifier
+// @include     https://*.bitrix24*/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var styles = " \
+.bx-layout-inner-left { \
+  padding: 0; \
+  width: 0; \
+} \
+ \
+.bx-layout-inner-left, #bx-im-bar { \
+  visibility: collapse; \
+} \
+ \
+.bx-layout-inner-table { \
+    padding-left: 3em; \
+    padding-right: 3em; \
+} \
+ \
+table.bx-layout-inner-inner-table td.bx-layout-inner-inner-cont { \
+  padding: 0; \
+} \
+ \
+td.bx-layout-inner-inner-cont { \
+  padding: 0; \
+} \
+";
+
+function addGlobalStyle(css) {
+  try {
+    var elmHead, elmStyle;
+    elmHead = document.getElementsByTagName('head')[0];
+    elmStyle = document.createElement('style');
+    elmStyle.type = 'text/css';
+    elmHead.appendChild(elmStyle);
+    elmStyle.innerHTML = css;
+  } catch (e) {
+    if (!document.styleSheets.length) {
+      document.createStyleSheet();
+    }
+    document.styleSheets[0].cssText += css;
+  }
+}
+
+addGlobalStyle(styles);
